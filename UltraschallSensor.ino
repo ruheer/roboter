fb46ac93534530345eeb7cd67d295a22a96af3d9
HolyShrek <142355449+HolyShrek@users.noreply.github.com>
2023-08-31 16:27:33 +0200

    Update UltraschallSensor.ino

--- UltraschallSensor.ino
+++ UltraschallSensor.ino
@@ -1,31 +1,19 @@
-int motorSpeed = 127;
-// Rechter Motor
-int enableRightMotor = 3;
-int rightMotorPin1 = 8;
-int rightMotorPin2 = 7;
-// Linker Motor
-int enableLeftMotor = 6;
-int leftMotorPin1 = 4;
-int leftMotorPin2 = 5;
-void setup()
-{
-pinMode(enableRightMotor, OUTPUT);
-pinMode(rightMotorPin1, OUTPUT);
-pinMode(rightMotorPin2, OUTPUT);
-pinMode(enableLeftMotor, OUTPUT);
-//Werkstatt zur Robotik Posten III: Motoren und Funktionen
-//Seite 7
-pinMode(leftMotorPin1, OUTPUT);
-pinMode(leftMotorPin2, OUTPUT);
-analogWrite(enableRightMotor, motorSpeed);
-analogWrite(enableLeftMotor, motorSpeed);
+# define echoPin 9
+# define trigPin 10
+long distance;
+
+void setup(){
+  pinMode(echoPin,INPUT);
+  pinMode(trigPin,OUTPUT);
+  Serial.begin(9600);
 }
-void loop()
-{
-digitalWrite(rightMotorPin1, HIGH);
-digitalWrite(leftMotorPin1, LOW);
-delay(2000);
-digitalWrite(rightMotorPin1, LOW);
-digitalWrite(leftMotorPin1, HIGH);
-delay(2000);
+
+void loop(){
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin,LOW);
+  distance = pulseIn(echoPin,HIGH)*0.017;
+  Serial.print(distance);
+  Serial.println(" cm");
+  delay(100);
 }
